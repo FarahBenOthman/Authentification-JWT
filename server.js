50f727edd9aa69c66fcd45f8f6b6faee1a8d2fa8
farah <farahbenothman2@gmail.com>
2023-04-02 12:20:19 +0100

    add

--- server.js
+++ server.js
@@ -7,7 +7,20 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 4000
 
+//const path=require("path")
+//css
+//app.use(express.static('public'))
+
+//app.set("view engine", "pug")
+//app.set("views", path.join(__dirname,"views"))
+
+//var home=require("./routes/home")
+
+
+
 app.use("/auth", require("./routes/authRoutes"))
+//app.use("/", home.home)
+//app.use("/home", home.home)
 app.use("/products", require("./routes/products"))
 
 app.listen(PORT, ()=> { console.log("Server is running on Port", + process.env.PORT)})
